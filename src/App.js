5bddd41c426db7201045160e3140530c5b8cdbdd
Anupam Gore <anupamgore@gmail.com>
2020-08-04 11:31:28 +0530

    feat(common): adding support for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  DiagramComponent
+} from "@syncfusion/ej2-react-diagrams"
+
+let nodes = [
+  {
+    id: "node1",
+    height: 100,
+    width: 100,
+    offsetX: 200,
+    offsetY: 100,
+    annotations: [
+      {
+        content: "React"
+      }
+    ]
+  },
+  {
+    id: "node2",
+    height: 100,
+    width: 100,
+    offsetX: 400,
+    offsetY: 300,
+    annotations: [
+      {
+        content: "SQL"
+      }
+    ]
+  },
+  {
+    id: "node3",
+    height: 100,
+    width: 100,
+    offsetX: 400,
+    offsetY: 450,
+    annotations: [
+      {
+        content: "Angular"
+      }
+    ]
+  },
+  {
+    id: "node4",
+    height: 100,
+    width: 100,
+    offsetX: 200,
+    offsetY: 250,
+    annotations: [
+      {
+        content: "Java"
+      }
+    ]
+  }
+];
+
+let connectors = [
+  {
+    id: "connector1",
+    sourceID: "node1",
+    targetID: "node2"
+  },
+  {
+    id: "connector2",
+    sourceID: "node2",
+    targetID: "node3"
+  },
+  {
+    id: "connector3",
+    sourceID: "node1",
+    targetID: "node4"
+  },
+  {
+    id: "connector4",
+    sourceID: "node1",
+    targetID: "node4"
+  }
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <DiagramComponent id="diagram" width={"100%"} height={"550px"} nodes={nodes}
+    connectors={connectors} />
     </div>
   );
 }
