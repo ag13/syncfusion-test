ffbd89cdefaa77d6e49ac21cc9407ab87b8072d2
Anupam Gore <anupamgore@gmail.com>
2020-08-21 11:40:40 +0530

    refactor(common): changes only for demo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useCallback } from 'react';
 import './App.css';
 import {
   DiagramComponent
@@ -10,112 +10,260 @@ import {
 
 function App() {
 
-  const [nodes, setNodes] = useState([])
-  const [connectors, setConnectors] = useState([])
+  // const [nodes, setNodes] = useState([])
+  // const [connectors, setConnectors] = useState([])
   let diagramInstance
 
-  useEffect(() => {
+  // useEffect(() => {
 
-    setTimeout(() => {
-      let nodes = [
-        {
-          id: "node1",
-          height: 100,
-          width: 100,
-          offsetX: 200,
-          offsetY: 100,
-          shape: {
-            type: 'HTML',
-          }
-        },
-        {
-          id: "node2",
-          height: 100,
-          width: 100,
-          offsetX: 400,
-          offsetY: 300,
-          annotations: [
-            {
-              content: "SQL"
-            }
-          ]
-        },
-        {
-          id: "node3",
-          height: 100,
-          width: 100,
-          offsetX: 400,
-          offsetY: 450,
-          annotations: [
-            {
-              content: "Angular"
-            }
-          ]
-        },
-        {
-          id: "node4",
-          height: 100,
-          width: 100,
-          offsetX: 200,
-          offsetY: 250,
-          annotations: [
-            {
-              content: "Java"
-            }
-          ]
-        }
-      ];
-      setNodes(nodes)
-    }, 4000 )
+  //   setTimeout(() => {
+  //     let nodes = [
+  //       {
+  //         id: "node1",
+  //         height: 100,
+  //         width: 100,
+  //         offsetX: 200,
+  //         offsetY: 100,
+  //         shape: {
+  //           type: 'HTML',
+  //         }
+  //       },
+  //       {
+  //         id: "node2",
+  //         height: 100,
+  //         width: 100,
+  //         offsetX: 400,
+  //         offsetY: 300,
+  //         annotations: [
+  //           {
+  //             content: "SQL"
+  //           }
+  //         ]
+  //       },
+  //       {
+  //         id: "node3",
+  //         height: 100,
+  //         width: 100,
+  //         offsetX: 400,
+  //         offsetY: 450,
+  //         annotations: [
+  //           {
+  //             content: "Angular"
+  //           }
+  //         ]
+  //       },
+  //       {
+  //         id: "node4",
+  //         height: 100,
+  //         width: 100,
+  //         offsetX: 200,
+  //         offsetY: 250,
+  //         annotations: [
+  //           {
+  //             content: "Java"
+  //           }
+  //         ]
+  //       }
+  //     ];
+  //     setNodes(nodes)
+  //   }, 4000 )
 
-    let connectors = [
-      {
-        id: "connector1",
-        sourceID: "node1",
-        targetID: "node2"
-      },
-      {
-        id: "connector2",
-        sourceID: "node2",
-        targetID: "node3"
-      },
-      {
-        id: "connector3",
-        sourceID: "node1",
-        targetID: "node4"
-      },
-      {
-        id: "connector4",
-        sourceID: "node1",
-        targetID: "node4"
+  //   let connectors = [
+  //     {
+  //       id: "connector1",
+  //       sourceID: "node1",
+  //       targetID: "node2"
+  //     },
+  //     {
+  //       id: "connector2",
+  //       sourceID: "node2",
+  //       targetID: "node3"
+  //     },
+  //     {
+  //       id: "connector3",
+  //       sourceID: "node1",
+  //       targetID: "node4"
+  //     },
+  //     {
+  //       id: "connector4",
+  //       sourceID: "node1",
+  //       targetID: "node4"
+  //     }
+  //   ];
+
+  //   setConnectors(connectors)
+  // }, [])
+
+  let nodes = [
+    {
+      id: "node1",
+      height: 100,
+      width: 100,
+      offsetX: 200,
+      offsetY: 100,
+      shape: {
+        type: 'HTML',
       }
-    ];
-
-    setConnectors(connectors)
-  }, [])
-
-  useEffect(() => {
-    if(diagramInstance && nodes.length){
-
-      // TODO - not able to get the customized template for first node!!
-
-      // diagramInstance.setNode
-      // console.log()
-      // diagramInstance.setNodeTemplate = setTemplate
-      
-      nodes.forEach(node => {
-        diagramInstance.add(node)
-      })
-      connectors.forEach(connector => {
-        diagramInstance.add(connector)
-      })
+    },
+    {
+      id: "node2",
+      height: 100,
+      width: 100,
+      offsetX: 400,
+      offsetY: 300,
+      annotations: [
+        {
+          content: "SQL"
+        }
+      ]
+    },
+    {
+      id: "node3",
+      height: 100,
+      width: 100,
+      offsetX: 400,
+      offsetY: 450,
+      annotations: [
+        {
+          content: "Angular"
+        }
+      ]
+    },
+    {
+      id: "node4",
+      height: 100,
+      width: 100,
+      offsetX: 200,
+      offsetY: 250,
+      annotations: [
+        {
+          content: "Java"
+        }
+      ]
     }
-  }, [diagramInstance, nodes, connectors])
+  ];
+  // setNodes(nodes)
+
+  let connectors = [
+    {
+      id: "connector1",
+      sourceID: "node1",
+      targetID: "node2"
+    },
+    {
+      id: "connector2",
+      sourceID: "node2",
+      targetID: "node3"
+    },
+    {
+      id: "connector3",
+      sourceID: "node1",
+      targetID: "node4"
+    },
+    {
+      id: "connector4",
+      sourceID: "node1",
+      targetID: "node4"
+    }
+  ];
+
+  // useEffect(() => {
+  //   if(diagramInstance){
+
+  //     let nodes = [
+  //       {
+  //         id: "node1",
+  //         height: 100,
+  //         width: 100,
+  //         offsetX: 200,
+  //         offsetY: 100,
+  //         shape: {
+  //           type: 'HTML',
+  //         }
+  //       },
+  //       {
+  //         id: "node2",
+  //         height: 100,
+  //         width: 100,
+  //         offsetX: 400,
+  //         offsetY: 300,
+  //         annotations: [
+  //           {
+  //             content: "SQL"
+  //           }
+  //         ]
+  //       },
+  //       {
+  //         id: "node3",
+  //         height: 100,
+  //         width: 100,
+  //         offsetX: 400,
+  //         offsetY: 450,
+  //         annotations: [
+  //           {
+  //             content: "Angular"
+  //           }
+  //         ]
+  //       },
+  //       {
+  //         id: "node4",
+  //         height: 100,
+  //         width: 100,
+  //         offsetX: 200,
+  //         offsetY: 250,
+  //         annotations: [
+  //           {
+  //             content: "Java"
+  //           }
+  //         ]
+  //       }
+  //     ];
+  //     // setNodes(nodes)
+
+  //     let connectors = [
+  //       {
+  //         id: "connector1",
+  //         sourceID: "node1",
+  //         targetID: "node2"
+  //       },
+  //       {
+  //         id: "connector2",
+  //         sourceID: "node2",
+  //         targetID: "node3"
+  //       },
+  //       {
+  //         id: "connector3",
+  //         sourceID: "node1",
+  //         targetID: "node4"
+  //       },
+  //       {
+  //         id: "connector4",
+  //         sourceID: "node1",
+  //         targetID: "node4"
+  //       }
+  //     ];
+  
+  //     // setConnectors(connectors)
+
+  //     // TODO - not able to get the customized template for first node!!
+
+  //     // diagramInstance.setNode
+  //     // console.log()
+  //     // diagramInstance.setNodeTemplate = setTemplate
+      
+  //     nodes.forEach(node => {
+  //       diagramInstance.add(node)
+  //     })
+  //     connectors.forEach(connector => {
+  //       diagramInstance.add(connector)
+  //     })
+  //   }
+  // }, [diagramInstance])
 
   const setTemplate = useCallback((props) => {
     console.log('props',props)
     if(props.id === 'node1'){
-      return (<button>Test</button>)
+      return (<button onClick={() => alert('test')}>Test</button>)
     }
   }, [])
   
@@ -126,6 +274,14 @@ function App() {
         <DiagramComponent id="diagram" width={"100%"} height={"550px"} 
       ref={diagram => (diagramInstance = diagram)}
       nodeTemplate={setTemplate}
+      created={(args) => {
+        nodes.forEach(node => {
+          diagramInstance.add(node)
+        })
+        connectors.forEach(connector => {
+          diagramInstance.add(connector)
+        })
+      }}
        />
       </div>
     // </React.StrictMode>
